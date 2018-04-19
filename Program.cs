a16c78e17dc6a26e12ffc8a924e418c4a6783b74
romannums <38524975+romannums@users.noreply.github.com>
2018-04-19 11:40:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_RimskaCisla
+{
+    class RimskeCislo
+    {
+        public long Cislo{ get; set; }
+
+        public long Zasobnik { get; set; }
+
+        public RimskeCislo(long cislo)
+        {
+            Cislo = cislo;
+        }
+
+        public long PoznejM(long cislo)
+        {
+            long M = 1000;
+            if (cislo >= M)
+            {
+                long zbytekM = cislo % M;
+                long podilM = (cislo - zbytekM) / M;
+                Zasobnik = zbytekM;
+                return podilM;
+            }
+            else
+            {
+                Zasobnik = cislo;
+                //return cislo;
+                return 0;
+            }
+        }
+
+        public long PoznejD(long cislo)
+        {
+            long D = 500;
+            if (cislo >= D)
+            {
+                long zbytekD = cislo % D;
+                long podilD = (cislo - zbytekD) / D;
+                Zasobnik = zbytekD;
+                return podilD;
+            }
+            else
+            {
+                Zasobnik = cislo;
+                //return cislo;
+                return 0;
+            }
+        }
+
+        public long PoznejC(long cislo)
+        {
+            long C = 100;
+            if (cislo >= C)
+            {
+                long zbytekC = cislo % C;
+                long podilC = (cislo - zbytekC) / C;
+                Zasobnik = zbytekC;
+                return podilC;
+            }
+            else
+            {
+                Zasobnik = cislo;
+                return 0;
+            }
+        }
+
+        public long PoznejL(long cislo)
+        {
+            long L = 50;
+            if (cislo >= L)
+            {
+                long zbytekL = cislo % L;
+                long podilL = (cislo - zbytekL) / L;
+                Zasobnik = zbytekL;
+                return podilL;
+            }
+            else
+            {
+                Zasobnik = cislo;
+                return 0;
+            }
+        }
+
+        public long PoznejX(long cislo)
+        {
+            long X = 10;
+            if (cislo >= X)
+            {
+                long zbytekX = cislo % X;
+                long podilX = (cislo - zbytekX) / X;
+                Zasobnik = zbytekX;
+                return podilX;
+            }
+            else
+            {
+                Zasobnik = cislo;
+                //return cislo;
+                return 0;
+            }
+        }
+
+        public long PoznejV(long cislo)
+        {
+            long V = 5;
+            if (cislo >= V)
+            {
+                long zbytekV = cislo % V;
+                long podilV = (cislo - zbytekV) / V;
+                Zasobnik = zbytekV;
+                return podilV;
+            }
+            else
+            {
+                Zasobnik = cislo;
+                return 0;
+            }
+        }
+
+        public long PoznejI(long cislo)
+        {
+            long I = 1;
+            if (cislo >= I)
+            {
+                return cislo;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pracovní plocha projektu
+            long cislo;
+            string rimskeCislo = "tady nic neni";
+            bool finish = false;
+            while (finish != true)
+            {
+                Console.Clear();
+                Console.WriteLine("Zadej cislo, ktere chces prevest:");
+                cislo = long.Parse(Console.ReadLine());
+                if(cislo < 0)
+                {
+                    finish = true;
+                    goto konec;
+                }
+                RimskeCislo c1 = new RimskeCislo(cislo);
+                Console.WriteLine($"Pro kontrolu: zadane cislo je {cislo} (ve tride je zapsano {c1.Cislo})");
+                Console.WriteLine();
+                Console.ReadKey();
+
+                Console.WriteLine("Tomuv chytry informacni system programu:");
+                Console.WriteLine();
+                if (c1.PoznejM(cislo) != cislo)
+                {
+                    Console.WriteLine($"Pocet vypsani M je: {c1.PoznejM(cislo)}");
+                }
+                else
+                {
+                    Console.WriteLine("Pocet vypsani M je: 0");
+                }
+
+                if (c1.PoznejD(cislo) != cislo)
+                {
+                    Console.WriteLine($"Pocet vypsani D je: {c1.PoznejD(cislo)}");
+                }
+                else
+                {
+                    Console.WriteLine("Pocet vypsani D je: 0");
+                }
+
+                if (c1.PoznejC(cislo) != cislo)
+                {
+                    Console.WriteLine($"Pocet vypsani C je: {c1.PoznejC(cislo)}");
+                }
+                else
+                {
+                    Console.WriteLine("Pocet vypsani C je: 0");
+                }
+
+                if (c1.PoznejL(cislo) != cislo)
+                {
+                    Console.WriteLine($"Pocet vypsani L je: {c1.PoznejL(cislo)}");
+                }
+                else
+                {
+                    Console.WriteLine("Pocet vypsani L je: 0");
+                }
+
+                if (c1.PoznejX(cislo) != cislo)
+                {
+                    Console.WriteLine($"Pocet vypsani X je: {c1.PoznejX(cislo)}");
+                }
+                else
+                {
+                    Console.WriteLine("Pocet vypsani X je: 0");
+                }
+
+                if (c1.PoznejV(cislo) != cislo)
+                {
+                    Console.WriteLine($"Pocet vypsani V je: {c1.PoznejV(cislo)}");
+                }
+                else
+                {
+                    Console.WriteLine("Pocet vypsani V je: 0");
+                }
+
+                Console.WriteLine($"Pocet vypsani I je: {c1.PoznejI(cislo)}");
+
+                Console.WriteLine();
+
+                //fáze parsování do výsledného řetězce
+                long pocetZnaku = 0;
+                char[] M = new char[c1.PoznejM(cislo)];
+                for (long m = 0; m < c1.PoznejM(cislo); m++)
+                {
+                    M[m] = 'M';
+                    rimskeCislo = string.Join("", M);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po M: {c1.Zasobnik}");
+                //Console.WriteLine($"Pocet znaku po M: {pocetZnaku}");
+
+                char[] D = new char[c1.PoznejD(cislo)];
+                for (long d = 0; d < c1.PoznejD(cislo); d++)
+                {
+                    D[d] = 'D';
+                    rimskeCislo = string.Join("", D);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po D: {c1.Zasobnik}");
+                //Console.WriteLine($"Pocet znaku po D: {pocetZnaku}");
+
+                char[] C = new char[c1.PoznejC(cislo)];
+                for (long c = 0; c < c1.PoznejC(cislo); c++)
+                {
+                    C[c] = 'C';
+                    rimskeCislo = string.Join("", C);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po C: {c1.Zasobnik}");
+
+                char[] L = new char[c1.PoznejL(cislo)];
+                for (long l = 0; l < c1.PoznejL(cislo); l++)
+                {
+                    L[l] = 'L';
+                    rimskeCislo = string.Join("", L);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po L: {c1.Zasobnik}");
+
+                char[] X = new char[c1.PoznejX(cislo)];
+                for (long x = 0; x < c1.PoznejX(cislo); x++)
+                {
+                    X[x] = 'X';
+                    rimskeCislo = string.Join("", X);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po X: {c1.Zasobnik}");
+                //Console.WriteLine($"Pocet znaku po X: {pocetZnaku}");
+
+                char[] V = new char[c1.PoznejV(cislo)];
+                for (long v = 0; v < c1.PoznejV(cislo); v++)
+                {
+                    V[v] = 'V';
+                    rimskeCislo = string.Join("", V);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po V: {c1.Zasobnik}");
+
+
+                char[] I = new char[c1.PoznejI(cislo)];
+                for (long i = 0; i < c1.PoznejI(cislo); i++)
+                {
+                    I[i] = 'I';
+                    rimskeCislo = string.Join("", I);
+                    pocetZnaku++;
+                }
+                cislo = c1.Zasobnik;
+                Console.WriteLine($"Zasobnik po I: {c1.Zasobnik}");
+                Console.WriteLine();
+
+                Console.WriteLine($"Nahled vysledneho retezce: {rimskeCislo}");
+
+                Console.ReadKey();
+            }
+
+            konec:
+
+            Console.Clear();
+            Console.WriteLine("Konec testovani...");
+            Console.ReadKey();
+            System.Environment.Exit(1);
+        }
+    }
+}
